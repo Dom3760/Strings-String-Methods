22e824b6dedc7de1980a3e7341df62c1954ced17
Dom3760 <142520274+Dom3760@users.noreply.github.com>
2023-11-30 10:29:32 -0800

    Add files via upload

---
+++ Strings & String Methods/Method's.js
@@ -0,0 +1,40 @@
+
+function Length(string)
+{
+    var counter = 0
+    for(let i = 0 ; i<string.length; i++)
+    {
+        counter ++
+    }
+    console.log(counter)
+}
+
+function slice ()
+{
+
+}
+
+function replace ()
+{
+    
+}
+
+replaceAll("yesSir")
+function replaceAll (newWord)
+{
+    var word = "no"
+    word = newWord
+    console.log(newWord)
+}
+
+concat("Yes","Sirrrrrr!")
+function concat (string1,string2) 
+{
+    var newString = string1+string2
+    console.log(newString)
+}
+
+function charAt ()
+{
+
+}
